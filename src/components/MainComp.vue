d04bfbbb012203fb1dc7b899cea1a48ce54262e6
Lucianor01 <lucianorosario02@gmail.com>
2023-05-03 15:52:22 +0200

    Struttura pagina

---
+++ src/components/MainComp.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: "MainComp",
+    data() {
+        return {
+        }
+    },
+    methods: {
+    },
+};
+</script>
+
+<!--! TEMPLATE -->
+<template>
+    <div>
+    </div>
+</template>
+
+<!--! STYLE -->
+<style scoped></style>
