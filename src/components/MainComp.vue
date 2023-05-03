15436abe1ebfb4fa2a33f379d984751812a52135
Lucianor01 <lucianorosario02@gmail.com>
2023-05-03 23:55:58 +0200

    MainComp dinamicamente

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -3,26 +3,32 @@ export default {
     name: "MainComp",
     data() {
         return {
+            path: "../../public/img/",
 
             linkBuy: [
                 {
                     name: "DIGITAL COMICS",
+                    alt: "Digital_Comics",
                     url: "buy-comics-digital-comics.png",
                 },
                 {
                     name: "DC MERCHANDISE",
+                    alt: "Dc_Merchandise",
                     url: "buy-comics-merchandise.png",
                 },
                 {
                     name: "SUBSCRIPTION",
+                    alt: "Subscription",
                     url: "buy-comics-subscriptions.png",
                 },
                 {
                     name: "COMIC SHOP LOCATOR",
+                    alt: "Comic_Shop_Locator",
                     url: "buy-comics-shop-locator.png",
                 },
                 {
                     name: "DC POWER VISA",
+                    alt: "Dc_Power_Visa",
                     url: "buy-dc-power-visa.svg",
                 },
             ]
@@ -46,46 +52,15 @@ export default {
         <section class="section_2 bg-primary">
             <div class="p-5 w-75 container">
                 <div class="row justify-content-between align-items-center">
-                    <!--? DIGITAL COMICS -->
-                    <div class="col-2">
-                        <a href="#">
-                            <img class="me-3 section2_img" src="../../public/img/buy-comics-digital-comics.png"
-                                alt="Digital_Comics">
-                            <span class="text-nowrap fs text-light">DIGITAL COMICS</span>
+
+                    <div v-for="(shop, index) in linkBuy" :key="index" class="col-2">
+                        <a :href="shop.url">
+                            <img class="me-3 section2_img" :src="path + shop.url" :alt="shop.alt"
+                                :id="(index == 3) ? 'section2_img' : ''">
+                            <span class="text-nowrap fs text-light">{{ shop.name }}</span>
                         </a>
                     </div>
-                    <!--? DC MERCHANDISE -->
-                    <div class="col-2">
-                        <a href="#">
-                            <img class="me-3 section2_img" src="../../public/img/buy-comics-merchandise.png"
-                                alt="Digital_Comics">
-                            <span class="text-nowrap fs text-light">DC MERCHANDISE</span>
-                        </a>
-                    </div>
-                    <!--? SUBSCRIPTION -->
-                    <div class="col-2">
-                        <a href="#">
-                            <img class="me-3 section2_img" src="../../public/img/buy-comics-subscriptions.png"
-                                alt="Digital_Comics">
-                            <span class="text-nowrap fs text-light">SUBSCRIPTION</span>
-                        </a>
-                    </div>
-                    <!--? COMIC SHOP LOCATOR  -->
-                    <div class="col-2">
-                        <a href="#">
-                            <img class="me-3" id="section2_img" src="../../public/img/buy-comics-shop-locator.png"
-                                alt="Digital_Comics">
-                            <span class="text-nowrap fs text-light">COMIC SHOP LOCATOR</span>
-                        </a>
-                    </div>
-                    <!--? DC POWER VISA  -->
-                    <div class="col-2">
-                        <a href="#">
-                            <img class="me-3 section2_img" src="../../public/img/buy-dc-power-visa.svg"
-                                alt="Digital_Comics">
-                            <span class="text-nowrap fs text-light">DC POWER VISA</span>
-                        </a>
-                    </div>
+
                 </div>
             </div>
         </section>
